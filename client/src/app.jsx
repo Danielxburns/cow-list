47dabc48893f1eb83206b49a6f2db74f1ed240f7
Danielxburns <danielburnsart@gmail.com>
2019-03-24 14:35:59 -0500

    add get request to componentDidMount method

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Spotlight from './spotlight.jsx';
 import AddCow from './addCow.jsx';
 import Herd from './herd.jsx';
@@ -10,12 +11,50 @@ class CowList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      testCows: ['John', 'Paul', 'George', 'Ringo'],
+      cows: []
       /*
       addCow needs to send data to db and reset the text fields
       herd needs display clicked cows in the spotlight
       */
     }
   }
+
+
+  componentDidMount() {
+    axios({
+      method: 'get',
+      url: '/api/cows',
+      responseType: 'application/json'
+    })
+    .then(res => {
+      console.log(`GET response=${JSON.parse(res.body)}`);
+      // this.setState({ cows: JSON.parse(res.body)})
+    })
+    .catch(err => {
+      console.log(`GET error=${err}`);
+    })
+  }
+
+
+  postCows() {
+    return axios({
+      method: 'post',
+      url: '/api/cows',
+      data: {
+        "cowname": "" /* from input field */,
+        "descript": ""/* from input field */
+      },
+      dataType: 'application/json'
+    })
+    .then(res => {
+      console.log(`POST response=${res}`)
+    })
+    .catch(err => {
+      console.log(`POST error=${err}`)
+    })
+  }
+
 
   handleSubmit(e){
 
@@ -41,7 +80,7 @@ class CowList extends React.Component {
         <h1 style={{textAlign: "center"}}>Cow List</h1>
         <Spotlight />
         <AddCow />
-        <Herd />
+        <Herd testCows={this.state.testCows}/>
       </div>
     )
   }
